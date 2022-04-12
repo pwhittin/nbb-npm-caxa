f49328a9babb71bb9c7e951ede832d40124c9677
Paul Whittington <pwhittington@ricelake.com>
2022-04-12 15:17:11 -0600

    remove format

--- src/nbb_npm_caxa/core.cljs
+++ src/nbb_npm_caxa/core.cljs
@@ -1,11 +1,6 @@
 (ns nbb-npm-caxa.core
-  (:require [goog.string :as gstring]
-            [goog.string.format]
-            ["readline-sync" :as rls]
+  (:require ["readline-sync" :as rls]
             ["sync-fetch$default" :as sf]))
-
-(defn format [fmt & args]
-  (apply gstring/format fmt args))
 
 (defn get-input [prompt]
   (rls/question prompt))
@@ -22,7 +17,7 @@
   (let [url (get-input "Enter url: ") ; try entering "https://book.babashka.org"
         url-text (.text (sf url))
         ids (re-seq #"id=\"[^\"]*\"" url-text)]
-    (println (format "There are %4d instances of id=\"" (count ids)))
+    (println "There are" (count ids) "instances of id=\"")
     (doseq [id (take 5 ids)]
       (println id))))
 
