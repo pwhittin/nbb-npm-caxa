9722ef9da49cc39dab6680824b0092cea261291b
Paul Whittington <pwhittington@ricelake.com>
2022-04-12 15:23:07 -0600

    remove sync-fetch; now the minimal version

--- src/nbb_npm_caxa/core.cljs
+++ src/nbb_npm_caxa/core.cljs
@@ -1,6 +1,5 @@
 (ns nbb-npm-caxa.core
-  (:require ["readline-sync" :as rls]
-            ["sync-fetch$default" :as sf]))
+  (:require ["readline-sync" :as rls]))
 
 (defn get-input [prompt]
   (rls/question prompt))
@@ -8,17 +7,8 @@
 (defn get-password-input [prompt]
   (rls/question prompt #js {:hideEchoBack true}))
 
-(defn base64-encode [in-string]
-  (as-> in-string s
-    (.from js/Buffer s)
-    (.toString s "base64")))
-
 (defn -main [& args]
-  (let [url (get-input "Enter url: ") ; try entering "https://book.babashka.org"
-        url-text (.text (sf url))
-        ids (re-seq #"id=\"[^\"]*\"" url-text)]
-    (println "There are" (count ids) "instances of id=\"")
-    (doseq [id (take 5 ids)]
-      (println id))))
+  (let [url (get-input "Enter url: ")] ; try entering "https://book.babashka.org"
+    (println "The url is:" url)))
 
 ;; (-main)
